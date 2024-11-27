97ccc0cdd07d25738cd3afe2a5160d6b34a3fcd3
Wylan Neely <wylanneely@gmail.com>
2024-11-26 17:14:30 -0800

    Pre Lesson 10

--- NotesProject/ViewControllers/NotesViewController.swift
+++ NotesProject/ViewControllers/NotesViewController.swift
@@ -23,20 +23,26 @@ class NotesViewController: UIViewController, UITableViewDelegate, UITableViewDat
     func setupTableView(){   // Set up the delegate and data source
         tableView.delegate = self
         tableView.dataSource = self
+        
+        //register nib
+        let nib = UINib(nibName: "NoteViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "NoteViewCell")
     }
     
     @IBOutlet weak var tableView: UITableView!
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let viewCell = tableView.dequeueReusableCell(withIdentifier: "notesViewCell", for: indexPath)
-        viewCell.backgroundColor = .blue
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NoteViewCell", for: indexPath) as? NoteViewCell else {
+                   return UITableViewCell()
+               }
         
-        return UITableViewCell()
+        
+        return cell
     }
     
     
